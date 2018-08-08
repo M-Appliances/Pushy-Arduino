c8187cc62d53a947e2c1ec8d766cfd6b8c24ce77
Thomas Cooper <tc@coope.biz>
2018-08-08 17:19:50 +0100

    Replace all of sketch into setup & out of loop

--- pb01_sketch/pb01z_Setup_Loop.ino
+++ pb01_sketch/pb01z_Setup_Loop.ino
@@ -1,40 +1,41 @@
 void setup() {
- 
-    //Set pinModes
-    pinMode(alertLED, OUTPUT);    
-    pinMode(successLED, OUTPUT);
-    pinMode(LED, OUTPUT);
-    pinMode(buzzer, OUTPUT);
-    esp_deep_sleep_enable_ext0_wakeup(GPIO_NUM_32, 0);
 
-    //Initialize serial and wait for port to open:
-    Serial.begin(115200);
-    delay(100);
+  //Set pinModes
+  pinMode(alertLED, OUTPUT);
+  pinMode(successLED, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  esp_deep_sleep_enable_ext0_wakeup(GPIO_NUM_32, 0);
+
+  //Initialize serial and wait for port to open:
+  Serial.begin(115200);
+  delay(100);
+
+  Serial.println("");
+  Serial.println("I'm awake!");
+
+  //Checking Battery
+  Serial.println("Checking my battery...");
+  battery_level();
+
+  //Connect To Wifi
+  if (sleepInit == 1) {
+    Serial.println("You've pressed the button...Lets talk to the Pantri API!");
+    connectToWifi();
+  } else {
+    Serial.println("Just a battery start - no button pressed!");
+  }
+
+  //Closing Down Until Button Press
+  sleepInit = 1;
+  Serial.println("Done... Night!");
+  delay(1000);
+  esp_deep_sleep_start();
 
 }
 
 void loop() {
 
-    Serial.println("");
-    Serial.println("I'm awake!");
 
-    //Checking Battery
-    Serial.println("Checking my battery...");
-    battery_level();
-
-    //Connect To Wifi
-    
-    if (sleepInit == 1) {
-        Serial.println("You've pressed the button...Lets talk to the Pantri API!");
-        connectToWifi();
-    } else {
-        Serial.println("Just a battery start - no button pressed!");
-    }
-
-        //Closing Down Until Button Press
-    sleepInit = 1;
-    Serial.println("Done... Night!");
-    delay(1000);
-    esp_deep_sleep_start();
 
 }
